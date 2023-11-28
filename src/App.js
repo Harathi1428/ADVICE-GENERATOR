e303684cb6526a73ad50a6038f0f81252e277871
Harathi1428 <harathiyenni@gmail.com>
2023-11-28 18:37:44 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
+const [advice,setAdvice] = useState("ADVICE..!");
+const generate=()=>{
+  axios.get("https://type.fit/api/quotes").then((response)=>{
+    console.log(response);
+    const data=response.data;
+    const adv=Math.floor(Math.random()*data.length);
+    setAdvice(data[adv].text);
+    console.log(data[adv].text);
+  }).catch((error)=>{
+    console.log(error);
+  })
+}
   return (
+    <section className='outer'>
+      <div className='bg-image'></div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="advice">{advice}</h1>
+      <button  className="btn" onClick={()=>generate()}>generate advice</button></div>
+    </section>
   );
 }
-
 export default App;
